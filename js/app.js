d684de3f1f8cbc98d0b318f2efda2893d4a10200
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-04-13 12:20:58 +0300

    change lab 2

--- js/app.js
+++ js/app.js
@@ -9,54 +9,54 @@ console.log(userName);
 alert('Please answer the following questions with yes or no');
 
 
-let userQuestion1 = prompt('Are you from Jordan?').toLowerCase();
+let userQuestion1 = prompt('Do I live in Jordan?').toLowerCase();
 if (userQuestion1==='Yes' && userQuestion1==='No'){
-  alert('answer :' + userQuestion1);
+  alert('Correct answer: Yes, I live in Jordan');
   console.log(userQuestion1);
 }else {
-  alert('answer :' + userQuestion1);
+  alert('Wrong answer, I live in Jordan');
   console.log(userQuestion1);
 
 }
 
 
-let userQuestion2 = prompt('Do you like ice cream?').toLowerCase();
+let userQuestion2 = prompt('Do I like ice cream?').toLowerCase();
 if (userQuestion2==='Yes' && userQuestion2==='No'){
-  alert('answer :' + userQuestion2);
+  alert('Correct answer: Yes, I like ice cream');
   console.log(userQuestion2);
 }else {
-  alert('answer :' + userQuestion2);
+  alert('Wrong answer, I like ice cream');
   console.log(userQuestion2);
 
 }
 
 
-let userQuestion3 = prompt('Do you like flowers?').toLowerCase();
+let userQuestion3 = prompt('Do I like flowers?').toLowerCase();
 if (userQuestion3==='Yes' && userQuestion2==='No'){
-  alert('answer :' + userQuestion3);
+  alert('Correct answer: Yes, I like flowers');
   console.log(userQuestion3);
 }else {
-  alert('answer :' + userQuestion3);
+  alert('Wrong answer, I like flowers');
   console.log(userQuestion3);
 
 }
 
 let userQuestion4 = prompt('Do you like watching TV?').toLowerCase();
 if (userQuestion4==='Yes' && userQuestion4==='No'){
-  alert('answer :' + userQuestion4);
+  alert('Wrong answer, I do not like flowers');
   console.log(userQuestion4);
 }else {
-  alert('answer :' + userQuestion4);
+  alert('Correct answer: Yes, I do not like watching TV');
   console.log(userQuestion4);
 
 }
 
 let userQuestion5 = prompt('Do you like animals ?').toLowerCase();
 if (userQuestion5==='Yes' && userQuestion5==='No'){
-  alert('answer :' + userQuestion4);
+  alert('Wrong answer, I do not like animals');
   console.log(userQuestion5);
 }else {
-  alert('answer :' + userQuestion5);
+  alert('Correct answer: Yes, I do not like animals');
   console.log(userQuestion5);
 }
 
