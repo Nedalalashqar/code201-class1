42c1ab91fc2e169191d2dd5ce88be92c517e5d2f
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-04-13 14:32:19 +0300

    lab3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # code201-class1
+
+## About Me
+
+> My name is Nedal Al-Ashqar i graduated from Zarqa University with a major in Management Information Systems, and I am very excited for this course for the sake of learning by two experts A growth mindset is believing in the power of yourself and your brain! We know our abilities develop when we try difficult things, use the right strategies, and don’t give up. So,a growth mindset is when we know, with practice, we will get better at something
+**Target**
+>Guessing game in order to get to know me closely
+***The method of work***
+>I will ask you some questions. You guess what the correct answer is (yes or no)
+> I have added the top ten foods that I like
--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 'use strict';
 
-
+let sum = 0;
 let userName = prompt('What is your name ?');
 alert('Welcome' + ' ' + userName + ' ' + 'to my page');
 console.log(userName);
@@ -9,55 +10,93 @@ console.log(userName);
 alert('Please answer the following questions with yes or no');
 
 
-let userQuestion1 = prompt('Do I live in Jordan?').toLowerCase();
-if (userQuestion1==='Yes' && userQuestion1==='No'){
+let userLive = prompt('Do I live in Jordan?').toLowerCase();
+if (userLive==='Yes' && userLive==='No'){
   alert('Correct answer: Yes, I live in Jordan');
-  console.log(userQuestion1);
+  console.log(userLive);
+  sum ++;
 }else {
   alert('Wrong answer, I live in Jordan');
-  console.log(userQuestion1);
+  console.log(userLive);
 
 }
 
 
-let userQuestion2 = prompt('Do I like ice cream?').toLowerCase();
-if (userQuestion2==='Yes' && userQuestion2==='No'){
+let userIcecream = prompt('Do I like ice cream?').toLowerCase();
+if (userIcecream==='Yes' && userIcecream==='No'){
   alert('Correct answer: Yes, I like ice cream');
-  console.log(userQuestion2);
+  console.log(userIcecream);
+  sum ++;
 }else {
   alert('Wrong answer, I like ice cream');
-  console.log(userQuestion2);
+  console.log(userIcecream);
 
 }
 
 
-let userQuestion3 = prompt('Do I like flowers?').toLowerCase();
-if (userQuestion3==='Yes' && userQuestion2==='No'){
+let userFlowers = prompt('Do I like flowers?').toLowerCase();
+if (userFlowers==='Yes' && userFlowers==='No'){
   alert('Correct answer: Yes, I like flowers');
-  console.log(userQuestion3);
+  console.log(userFlowers);
+  sum ++;
 }else {
   alert('Wrong answer, I like flowers');
-  console.log(userQuestion3);
+  console.log(userFlowers);
 
 }
 
-let userQuestion4 = prompt('Do you like watching TV?').toLowerCase();
-if (userQuestion4==='Yes' && userQuestion4==='No'){
+let userTv = prompt('Do you like watching TV?').toLowerCase();
+if (userTv==='Yes' && userTv==='No'){
   alert('Wrong answer, I do not like flowers');
-  console.log(userQuestion4);
+  console.log(userTv);
 }else {
   alert('Correct answer: Yes, I do not like watching TV');
-  console.log(userQuestion4);
-
+  console.log(userTv);
+  sum ++;
 }
 
-let userQuestion5 = prompt('Do you like animals ?').toLowerCase();
-if (userQuestion5==='Yes' && userQuestion5==='No'){
+let userAnimals = prompt('Do you like animals ?').toLowerCase();
+if (userAnimals==='Yes' && userAnimals==='No'){
   alert('Wrong answer, I do not like animals');
-  console.log(userQuestion5);
+  console.log(userAnimals);
 }else {
   alert('Correct answer: Yes, I do not like animals');
-  console.log(userQuestion5);
+  console.log(userAnimals);
+  sum ++;
 }
 
-alert('Tank you' + ' ' + userName + 'to answer the questions');
+for (let i =0 ; i < 4 ; i++){
+  let num = prompt('What is my favorite number?').toLowerCase();
+  while (num==='5'){
+    alert('Well, this is my favorite number, you are amazing');
+    console.log(num);
+    sum ++;
+  }if (num > 5) {
+    alert('too high');
+  } else {
+    alert('too low');
+  }
+}
+alert('My favorite number is 5');
+
+let userColor = ['red' + 'black' + 'blue'];
+
+for (let i =0 ; i < 6 ; i++){
+  let pasColor = prompt('What are my favorite colors');
+  while (userColor ===pasColor){
+    alert('Well, this is my favorite Color, you are amazing');
+    console.log(pasColor);
+    sum ++;
+  }if (userColor !== pasColor) {
+    alert('too This is not my favorite color');
+  } else {
+    alert('You are getting close to the correct answero low');
+  }
+}
+alert(userColor);
+console.log(userColor);
+
+
+alert('Tank you' + ' ' + userName + ' ' + 'to answer the questions');
+
+alert('The number of correct answers' + ' ' + sum);
